eb6e32fcaa5136e26d1d85a4c98ca8ff067821c9
christian pritzl <christian.pritzl@outlook.de>
2023-01-02 11:39:29 +0100

    add overview-block

--- frontend/frontend/src/views/RepositoryView.vue
+++ frontend/frontend/src/views/RepositoryView.vue
@@ -1,13 +1,66 @@
 <script>
 export default {
   props: ['id'],
-  data() { }
+  data() {
+    return {
+      overviewBlock: 'overview-block',
+      overviewElementContainer: 'overview-element-container',
+      overviewElement: 'overview-element',
+      mockRepo: { id:  0,
+        name: "Repo1",
+        lastScan: '2022-12-12:00:08:',
+        findingsAmount: 14,
+        falsePositivesAmount: 4,
+        toReviewAmount: 5
+      }
+    }
+  },
+  methods: {
+    getRepoData(repoID) {
+      return {
+        repoID: this.mockRepo.id,
+        repoName: this.mockRepo.name,
+        lastScan: this.mockRepo.lastScan,
+        findingsAmount: this.mockRepo.findingsAmount,
+        falsePositivesAmount: this.mockRepo.falsePositivesAmount,
+        toReviewAmount: this.mockRepo.toReviewAmount
+      }
+    }
 
+  }
 }
 </script>
 
 
 <template>
-  <h1>Hello from Repository-View</h1>
-  <h2> Repo {{ id }}</h2>
+  <div :class="overviewBlock" >
+    <h2>{{ getRepoData().repoName }}</h2>
+    <v-container :class="overviewElementContainer" fluid>
+      <v-row no-gutters :class="overviewElement">
+        <v-col>
+          <v-card title="#Repos" :text="getRepoData().findingsAmount"></v-card>
+        </v-col>
+        <v-col>
+          <v-card title="#Falsch-Positive" :text="getRepoData().falsePositivesAmount"></v-card>
+        </v-col>
+        <v-col>
+          <v-card title="#Findings-To-Review" :text="getRepoData().toReviewAmount"></v-card>
+        </v-col>
+        <v-col>
+          <v-card title="LastScan" :text="getRepoData().lastScan">
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
+
+<style>
+.overview-block {
+  margin: 3em;
+}
+
+.overview-element {
+  margin: 2em
+}
+</style>
