7cd99dbff711d082c5e8158429c529402bfc9476
christian pritzl <christian.pritzl@outlook.de>
2023-01-04 16:19:14 +0100

    404-default-route

--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import RepositoryView from '@/views/RepositoryView.vue'
-import LoginView from '@/views/LoginView'
+import LoginView from '@/views/LoginView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import {useTokenStore} from "@/store/token";
 
 const routes = [
   {
@@ -20,10 +22,34 @@ const routes = [
     component: RepositoryView,
     props: true,
     auth: true
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFoundView',
+    component: NotFoundView
   }
 ]
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+const publicPages = ['/login'];
+
+router.beforeEach((to, from, next) => {
+
+  const authRequired = !publicPages.includes((to.path))
+  const loggedIn = useTokenStore().token
+  console.log("beforeEach -> " + loggedIn)
+
+
+  if (authRequired && !loggedIn) {
+    next('/login')
+  } else if(to.path === '/login' && loggedIn ){
+    next('/')
+  } else {
+    next()
+  }
+})
+
 export default router
