a5840156a94f0cc8c0a77a1af012a0a2f2d72df9
christian pritzl <christian.pritzl@outlook.de>
2023-01-05 13:08:37 +0100

    enable cors

--- backend/app/server/app.py
+++ backend/app/server/app.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from app.server.routes.findings_route import router as FindingRouter
 from app.server.routes.user_route import router as UserRouter
 from app.server.routes.login_route import router as LoginRouter
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(FindingRouter, tags=["Finding"], prefix='/finding')
 app.include_router(UserRouter, tags=["User"], prefix='/user')
