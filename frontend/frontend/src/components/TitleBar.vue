034e829c08502cef22a9c45b5e927b2ae25680e7
christian pritzl <christian.pritzl@outlook.de>
2023-01-05 10:06:08 +0100

    todo import/export function

--- frontend/frontend/src/components/TitleBar.vue
+++ frontend/frontend/src/components/TitleBar.vue
@@ -16,6 +16,7 @@ export default {
   <v-toolbar-title>Welcome to FP-Marker</v-toolbar-title>
     <v-spacer></v-spacer>
     <div :class="navigationButtonGroup">
+      <v-btn :class="navigationButton" variant="tonal" color="info">Todo: Import/Export</v-btn>
       <v-btn :class="navigationButton" variant="tonal" color="info">Repositories-View</v-btn>
       <v-btn :class="navigationButton" variant="tonal" color="info">Files-View</v-btn>
       <v-btn :class="navigationButton" variant="tonal" color="success">Logout</v-btn>
