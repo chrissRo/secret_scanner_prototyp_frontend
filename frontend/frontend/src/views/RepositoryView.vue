a2d174cf77f118cd24a55c03a94f840a81ca280b
christian pritzl <christian.pritzl@outlook.de>
2023-01-16 16:34:21 +0100

    first dialog fixed -> need array because dialog is now list-element "false-positive edit" dialog still needs to be fixed

--- frontend/frontend/src/views/RepositoryView.vue
+++ frontend/frontend/src/views/RepositoryView.vue
@@ -11,69 +11,24 @@ export default {
   },
   created() {
     this.getOverviewData()
+    this.getFindingsList()
   },
   props: {
     id: String,
     lastScan: String
   },
-
   data() {
-    let mockScanResult= {
-      "id": "63a03d0b461bc420de4e638c",
-      "scannerType": 0,
-      "scannerVersion": "8.15",
-      "inputType": 0,
-      "repositoryPath": ".",
-      "repositoryName": "applications",
-      "scanStartTime": "2022-12-16T00:00:00",
-      "scanEndTime": "2022-12-16T00:00:00",
-      "save_date": "2022-12-19T11:29:31.810344",
-      "resultRaw": {
-        "Description": "Generic API Key",
-        "StartLine": 140,
-        "EndLine": 141,
-        "StartColumn": 8,
-        "EndColumn": 1,
-        "Match": "apiGroup: rbac.authorization.k8s.io",
-        "Secret": "rbac.authorization.k8s.io",
-        "File": "weave-scope/scope.yaml",
-        "Commit": "d1925d8548b86acfeb51912909f92756eb687412",
-        "Entropy": 3.7230742,
-        "Author": "Tibor Pilz",
-        "Email": "tibor@pilz.berlin",
-        "Date": "2021-09-10T15:57:37+00:00",
-        "Message": "Add weave",
-        "Tags": [],
-        "RuleID": "generic-api-key",
-        "Fingerprint": "d1925d8548b86acfeb51912909f92756eb687412:weave-scope/scope.yaml:generic-api-key:140"
-      },
-      "falsePositive": {
-        "isFalsePositive": true,
-        "justification": "weil halt",
-        "change_date": "2022-12-19T12:43:06.801898"
-      }
-    }
-
     return {
       headerBlock: 'header-block',
       overviewBlock: 'overview-block',
       overviewElementContainer: 'overview-element-container',
       overviewElement: 'overview-element',
       listItem: 'list-item',
-      findingsList: 'findings-list',
+      findingsListContainer: 'findings-list-container',
       findingsListSearchBar: 'findings-list-search-bar',
-      mockRepo: {
-        id: 0,
-        name: "Repo1",
-        lastScan: '2022-12-12:00:08:',
-        findingsAmount: 14,
-        falsePositivesAmount: 4,
-        toReviewAmount: 5,
-        },
-      mockScanResult,
-      mockScanResults: [mockScanResult],
-      rawResultDialog: false,
-      falsePositiveDialog: false,
+      findingsList: [],
+      rawResultDialog: [],
+      falsePositiveDialog: false, // TODO fix like rawResultDialog
       falsePositiveReadOnly: true,
       searchInput: '',
       overviewData: {}
@@ -83,7 +38,6 @@ export default {
     async getOverviewData() {
       this.$axios.defaults.headers.Authorization = `Bearer ${this.tokenStore.token}`
       this.$axios.get(`/finding/repository/${this.id}/count/`).then((res) => {
-        console.log(res.data)
         this.overviewData = {
           documentsAmount: String(res.data['data']['total_number_of_documents']),
           falsePositivesAmount: String(res.data['data']['total_number_of_false_positives']),
@@ -91,6 +45,13 @@ export default {
           toReviewAmount: String(res.data['data']['total_number_of_todos']),
           lastScanData: this.lastScan
         }
+        // eslint-disable-next-line no-unused-vars
+      }).catch((err) => {/*pass to global error handler*/})
+    },
+    async getFindingsList() {
+      this.$axios.defaults.headers.Authorization = `Bearer ${this.tokenStore.token}`
+      this.$axios.get(`/finding/repository/${this.id}/`).then((res) => {
+        this.findingsList = res.data['data']
         // eslint-disable-next-line no-unused-vars
       }).catch((err) => {/*pass to global error handler*/})
     },
@@ -108,11 +69,11 @@ export default {
     }
   },
   computed: {
-    filteredScanResults() {
-      if (this.searchInput) {
-        return this.mockScanResults.filter((result) => JSON.stringify(result).toLowerCase().includes(this.searchInput.toLowerCase()))
+    filteredFindingsList() {
+      if (!this.searchInput) {
+        return this.findingsList
       } else {
-        return this.mockScanResults
+        return this.findingsList.filter((result) => JSON.stringify(result).toLowerCase().includes(this.searchInput.toLowerCase()))
       }
     }
   }
@@ -147,12 +108,10 @@ export default {
         </v-col>
       </v-row>
     </v-container>
-
   </div>
   <v-divider inset></v-divider>
 
-
-  <div :class="findingsList">
+  <div :class="findingsListContainer">
     <v-toolbar color="primary">
       <v-toolbar-title class="text-h5">Findings List</v-toolbar-title>
     </v-toolbar>
@@ -172,31 +131,33 @@ export default {
       <v-card class="mx-auto">
         <v-list>
             <v-list-item
-              v-for="scanResult in filteredScanResults"
-              :key="scanResult.id"
+              v-for="(scanResult, index) in filteredFindingsList"
+              :key="scanResult._id"
               :class="listItem"
             >
               <v-container fluid>
                 <v-row>
-                  <v-col>{{ scanResult.resultRaw.File }}</v-col>
+                  <v-col>{{scanResult.resultRaw.File}}</v-col>
                   <v-col>{{scanResult.resultRaw.Match}}</v-col>
                   <v-col>{{scanResult.save_date}}</v-col>
                   <v-col>
-                        <v-btn><v-icon>mdi-details</v-icon> Show Raw Result
-                          <v-dialog v-model="rawResultDialog" activator="parent" width="50%">
-                            <v-card >
-                            <v-card-text>
-                              <pre style="white-space:pre-wrap;">{{scanResult.resultRaw}}</pre>
-                            </v-card-text>
-                              <v-divider></v-divider>
-                            <v-card-actions>
-                              <v-col class="text-right">
-                                <v-btn color="primary" @click="rawResultDialog = false">Close</v-btn>
-                              </v-col>
-                            </v-card-actions>
-                          </v-card>
-                          </v-dialog>
-                        </v-btn>
+                    <v-dialog v-model="rawResultDialog[index]" width="50%">
+                      <template v-slot:activator="{props}">
+                        <v-btn v-bind="props"><v-icon>mdi-details</v-icon> Show Raw Result</v-btn>
+                      </template>
+                      <v-card >
+                        <v-card-text>
+                          <pre style="white-space:pre-wrap;">{{scanResult.resultRaw}}</pre>
+                        </v-card-text>
+                        <v-divider></v-divider>
+                        <v-card-actions>
+                          <v-col class="text-right">
+                            <v-btn color="primary" @click="rawResultDialog[index] = false">Close</v-btn>
+                          </v-col>
+                        </v-card-actions>
+                      </v-card>
+                    </v-dialog>
+
                   </v-col>
                   <v-col>
                     <v-btn>
@@ -225,7 +186,7 @@ export default {
                           <v-divider></v-divider>
                           <v-card-actions>
                             <v-col class="text-right">
-                              <v-btn v-if="falsePositiveReadOnly === true"  color="primary" @click="editFalsePositive">Edit</v-btn>
+                              <v-btn v-if="falsePositiveReadOnly === true" color="primary" @click="editFalsePositive">Edit</v-btn>
                               <v-btn v-else  color="primary" @click="updateFalsePositive({falsePositive: scanResult.falsePositive, id: scanResult.id})">Save</v-btn>
                               <v-btn color="primary" @click="falsePositiveDialog = false">Close</v-btn>
                             </v-col>
@@ -257,7 +218,7 @@ export default {
   margin: 0.25em
 }
 
-.findings-list {
+.findings-list-container {
   margin: 3em;
 }
 
