4ed8573b4ad99f47169cc106d16b4d1c16a2d65c
christian pritzl <christian.pritzl@outlook.de>
2023-03-07 21:06:49 +0100

    remove https

--- frontend/frontend/src/api/axiosClient.js
+++ frontend/frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import router from "@/router";
 
 export const axiosClient = axios.create({
-  baseURL: 'https://localhost:8000',
+  baseURL: 'http://localhost:8000',
   timeout: 1000
 })
 
--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -36,7 +36,6 @@ export default defineConfig({
     ],
   },
   server: {
-    port: 3000,
-    https: true
+    port: 3000
   }
 })
