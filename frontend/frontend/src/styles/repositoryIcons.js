9c571327964d87976d2eea546a98ab35d66c7140
christian pritzl <christian.pritzl@outlook.de>
2023-03-10 16:12:31 +0100

    add link to repository

---
+++ frontend/frontend/src/styles/repositoryIcons.js
@@ -0,0 +1,8 @@
+const repositoryIcons = {
+  'bitbucket': 'mdi-bitbucket',
+  'gitlab': 'mdi-gitlab',
+  'github': 'mdi-github',
+  'default': 'mdi-source-repository'
+}
+
+export default repositoryIcons
